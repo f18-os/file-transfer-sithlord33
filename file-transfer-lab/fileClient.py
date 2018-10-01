c9bc091f8f1748300746510d971ca13262b3edd2
Ricardo Jimenez <ricardotodd33@hotmail.com>
2018-10-01 17:28:15 -0600

    created fileClient.py and fileServer.py

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
---
+++ file-transfer-lab/fileServer.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+
+sys.path.append("../lib")       # for params
+import sys, re, socket, params
